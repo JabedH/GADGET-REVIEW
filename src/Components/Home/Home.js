d8c6191194f9115d2e9c0910c16fbd08c5cbc946
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-04 02:13:18 +0600

    added home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import "./Home.css";
+import img from "../../img/1.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>
-        Gadget Review <br /> Your product is here{" "}
-      </h1>
+    <div className="home">
+      <div>
+        <h1>
+          CHOOSE YOUR GADGET <br /> <span>GIVE US HAPPINESS</span>
+        </h1>
+        <p className="text-neutral-500 mt-5">
+          This site is for garage reviewing and selecting products. <br /> If
+          you need any good product you can search for anything <br /> from this
+          side and buy it. Happy Shopping.
+        </p>
+      </div>
+      <div>
+        <img src={img} alt="" />
+      </div>
     </div>
   );
 };
