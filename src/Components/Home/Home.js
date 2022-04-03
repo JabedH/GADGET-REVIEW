5ccbc82a762fabaa7e0d054533f40045c72b32ca
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-04 02:24:08 +0600

    customer reviews

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,19 +4,28 @@ import img from "../../img/1.jpg";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div>
-        <h1>
-          CHOOSE YOUR GADGET <br /> <span>GIVE US HAPPINESS</span>
-        </h1>
-        <p className="text-neutral-500 mt-5">
-          This site is for garage reviewing and selecting products. <br /> If
-          you need any good product you can search for anything <br /> from this
-          side and buy it. Happy Shopping.
-        </p>
+    <div>
+      <div className="home">
+        <div>
+          {" "}
+          <h1>
+            CHOOSE YOUR GADGET <br /> <span>GIVE US HAPPINESS</span>
+          </h1>
+          <p className="text-neutral-500 mt-5">
+            This site is for garage reviewing and selecting products. <br /> If
+            you need any good product you can search for anything <br /> from
+            this side and buy it. Happy Shopping.
+          </p>
+          <button className="border p-5 rounded-md hover:bg-slate-300 mt-5">
+            More Products
+          </button>
+        </div>
+        <div>
+          <img src={img} alt="" />
+        </div>
       </div>
-      <div>
-        <img src={img} alt="" />
+      <div className="reviews">
+        <h1>Customer Reviews</h1>
       </div>
     </div>
   );
