4266eceee13f4b1a61caf0a2f018d752f716e442
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-04 13:20:37 +0600

    added button

--- src/Components/CustomerReview/CustomerReview.js
+++ src/Components/CustomerReview/CustomerReview.js
@@ -3,6 +3,7 @@ import UseProduct from "../Hooks/UseProduct";
 import img from "../../img/1.jpg";
 import CustomerReviewData from "../CustomerReviewData/CustomerReviewData";
 import "./CustomerReview.css";
+import { Link } from "react-router-dom";
 
 const CustomerReview = () => {
   const [products, setProducts] = UseProduct();
@@ -10,10 +11,19 @@ const CustomerReview = () => {
   console.log(products);
 
   return (
-    <div className="customerReview">
-      {myProducts.map((product) => (
-        <CustomerReviewData key={product.id} product={product} />
-      ))}
+    <div className="mb-10">
+      <div className="customerReview">
+        {myProducts.map((product) => (
+          <CustomerReviewData key={product.id} product={product} />
+        ))}
+      </div>
+
+      <Link
+        to="/Reviews"
+        className="seeReviews p-3 rounded-lg bg-slate-300 hover:bg-blue-500 hover:text-white"
+      >
+        See all Reviews
+      </Link>
     </div>
   );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="home">
         <div>
           {" "}
-          <h1>
+          <h1 className="text-blue-500">
             CHOOSE YOUR GADGET <br /> <span>GIVE US HAPPINESS</span>
           </h1>
           <p className="text-neutral-500 mt-5">
@@ -17,7 +17,7 @@ const Home = () => {
             you need any good product you can search for anything <br /> from
             this side and buy it. Happy Shopping.
           </p>
-          <button className="border p-5 rounded-md hover:bg-slate-300 mt-5">
+          <button className="border p-5 rounded-md bg-slate-300 hover:bg-blue-500 hover:text-white mt-5">
             More Products
           </button>
         </div>
